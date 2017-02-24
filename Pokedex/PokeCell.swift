8f5cba599e5da2f5035e4ffc58eb8a551e23254d
Aman Vaid <amanvaid94@gmail.com>
2017-02-24 00:41:59 -0800

    Added App Icon, created UI, and filled Cells

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Aman Vaid on 2/23/17.
+//  Copyright © 2017 Aman Vaid. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Aman Vaid on 2/23/17.
+//  Copyright © 2017 Aman Vaid. All rights reserved.
+//
+
+import Foundation
