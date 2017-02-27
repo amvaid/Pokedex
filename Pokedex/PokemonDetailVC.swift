da442baa0f1a28a5dc799bf0e7459bafdf16df8e
Aman Vaid <amanvaid94@gmail.com>
2017-02-26 20:15:18 -0800

    Added functionality to the back button when in the details screen. Also variable declaration for the items in the details screen

---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,38 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Aman Vaid on 2/25/17.
+//  Copyright © 2017 Aman Vaid. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    var pokemon: Pokemon!
+    
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameLbl.text = pokemon.name
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
