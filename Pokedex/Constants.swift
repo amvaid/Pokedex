c46dd6904c589b487b243c2aabeeff25f837c952
Aman Vaid <amanvaid94@gmail.com>
2017-05-21 13:25:40 -0700

    Most recent changes

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Aman Vaid on 2/26/17.
+//  Copyright © 2017 Aman Vaid. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
